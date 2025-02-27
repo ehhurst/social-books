d549a73dc3b284f16f1141462944a2abb3cd213a
ehurst96 <ehurst96@vt.edu>
2025-02-26 21:14:05 -0500

    Footer content and presentation

--- frontend/src/components/AppFooter.tsx
+++ frontend/src/components/AppFooter.tsx
@@ -1,6 +1,19 @@
+import {Link} from 'react-router-dom'
+import '../assets/css/AppFooter.css'
+import '../assets/css/global.css'
 
 function AppFooter() {
-    return(<></>);
+    return(
+        <footer>
+            <ul>
+                <li><Link to='/'>Home           </Link><p> | </p></li>
+                <li><Link to='#'>Books         </Link><p> | </p> </li>
+                <li><Link to='#'>Book Clubs     </Link><p> | </p></li>
+                <li><Link to='#'>Competitions   </Link><p> | </p></li>
+                <li><Link to='#'>Contact        </Link>          </li>
+            </ul>
+        </footer>
+    );
 }
 
 export default AppFooter;
