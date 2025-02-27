04fbab4cb9f0eb0fc469297924813f9fa13f2bde
ehurst96 <ehurst96@vt.edu>
2025-02-27 16:36:06 -0500

    update login component formatting

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -9,7 +9,7 @@ function Login() {
     const [errorMessage, setErrorMessage] = useState("");
     const navigate = useNavigate();
 
-
+    // handles submission of form data
     async function handleSubmit(event:FormEvent) {
         event.preventDefault();
         if (formData === "") {setErrorMessage("Username is required. Please enter your username.")}
@@ -33,8 +33,8 @@ function Login() {
         <div id='container'>
             <div id='login-form'>
                 <form
-                onSubmit={(event)=>handleSubmit(event)}
-                method='post'>
+                    onSubmit={(event)=>handleSubmit(event)}
+                    method='post'>
                     <label htmlFor='username'>Username</label>
                         <input 
                             type='text'
@@ -44,21 +44,18 @@ function Login() {
                             value={formData}
                             onChange={(event) => setFormData(event.target.value)}
                             />
-
                     <section>
                         {errorMessage !== "" ? 
                         <><p id='error-message' aria-live='assertive'>{errorMessage}</p>
                         </> : <></> } 
                     </section>
                     <button className='primary'>Sign In</button>
-            </form>
-            <div id='alt'>
-                <p>Don't have an account? </p>
-                <Link to='/register'>Sign Up</Link>
+                </form>
+                <div id='alt'>
+                    <p>Don't have an account? </p>
+                    <Link to='/register'>Sign Up</Link>
+                </div>
             </div>
-            </div>
-            
-
         </div>
     );
 }
