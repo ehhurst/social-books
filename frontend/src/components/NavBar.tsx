1d57515bf0bb096178e50d165b7519fc4a51f688
ehurst96 <ehurst96@vt.edu>
2025-02-26 22:26:35 -0500

    navigation bar updates

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,13 +3,13 @@ import '../assets/css/NavBar.css'
 
 function NavBar() {
     return(            
-    <ul>
-        <li><Link to='/'>Home</Link>        <p> | </p></li>
-        <li><Link to='#'>Books</Link>       <p> | </p> </li>
-        <li><Link to='#'>Book Clubs</Link>  <p> | </p></li>
-        <li><Link to='#'>Competitions</Link><p> | </p></li>
-        <li><Link to='#'>Contact</Link>               </li>
-    </ul>
+        <ul>
+            <li><Link to='/'>Home</Link>         <p> | </p></li>
+            <li><Link to='#'>Books</Link>        <p> | </p> </li>
+            <li><Link to='#'>Book Clubs</Link>   <p> | </p></li>
+            <li><Link to='#'>Competitions</Link> <p> | </p></li>
+            <li><Link to='#'>Contact</Link>               </li>
+        </ul>
     );
 }
 
