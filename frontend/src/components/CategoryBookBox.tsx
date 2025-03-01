cae9c4f47240d366a42b632a11e16f3cdbcddcb7
ehurst96 <ehurst96@vt.edu>
2025-02-28 19:03:21 -0500

    book item content to be displayed on page of all books

---
+++ frontend/src/components/CategoryBookBox.tsx
@@ -0,0 +1,26 @@
+import { BookItem } from "../types";
+import { Link } from "react-router-dom";
+
+
+function CategoryBookBox(props:BookItem) {
+    return(
+        <div id="category-book-box">
+            <Link to='/${isbn}'>
+                <div id="book-image">
+                    <img src={`https://covers.openlibrary.org/b/isbn/${props.isbn}-S.jpg`} alt="Book cover image"/>
+                </div>
+                <div id="book-info">
+                    <div id='title-author'>
+                        <h3>{props.title}</h3>
+                        <h4>{props.author_name}</h4>
+                    </div>
+                    <p>{props.description}</p>
+                </div>
+            </Link>
+
+        </div>
+    );
+
+}
+
+export default CategoryBookBox;
---
+++ frontend/src/components/CategoryBookListPage.tsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios'
+import {BookItem} from '../types'
+import CategoryBookBox from "./CategoryBookBox";
+
+function CategoryBookListPage() {
+    const {category} = useParams();
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        axios.get('...api/categories/name/${category}/books')
+        .then((response) => setBooks(response.data))
+        .catch(console.error)
+    }, [category]);
+
+
+    return(
+        <main>
+            <ul id="book-list">
+                {books.map((book:BookItem) => 
+                    <CategoryBookBox isbn={book.isbn} title={book.title} author_name={book.author_name} description={book.description} readTime={book.readTime}/>)}
+            </ul>
+        </main>
+    )
+
+}
+
+export default CategoryBookListPage;
