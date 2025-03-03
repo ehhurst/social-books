e2ec14e52407bc2846d584553d0a96cf72938617
Nithin Alluru <104234004+nithinalluri@users.noreply.github.com>
2025-03-04 01:15:28 +0530

    implemented the search API. search queries: general search query, search by title, search by author, search by subject. Default reponse limit if not provided as param is 5 books.

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from book_api import fetch_books_from_api, parse_books
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to communicate with backend
@@ -19,6 +20,27 @@ def parse_book():
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+@app.route('/search', methods=['GET'])
+def search_books():
+    """
+    Flask route to handle book search requests.
+    
+    Returns:
+        Response: JSON response containing search results or an error message.
+    """
+    query = request.args.get('q')
+    title = request.args.get('title')
+    author = request.args.get('author')
+    subject = request.args.get('subject')
+
+    if not (query or title or author or subject):
+        return jsonify({'error': 'Missing search parameter'}), 400
+
+    # Use the fetch_books_from_api function to get the search results
+    data = fetch_books_from_api(query=query, title=title, author=author, subject=subject)
+    parsed_data = parse_books(data)
+    return jsonify(parsed_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
