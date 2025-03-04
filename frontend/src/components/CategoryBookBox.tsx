fc8a413bb5a2ca3fbef6e4e8503fb11755f9e5ac
ehurst96 <ehurst96@vt.edu>
2025-03-03 20:30:52 -0500

    created card to display book information on the browse books page

--- frontend/src/components/CategoryBookBox.tsx
+++ frontend/src/components/CategoryBookBox.tsx
@@ -1,23 +1,29 @@
 import { BookItem } from "../types";
 import { Link } from "react-router-dom";
+import '../assets/css/CategoryBookBox.css'
+
 
 
 function CategoryBookBox(props:BookItem) {
+    const preview = props.description.slice(0, 150);
+
+
     return(
         <div id="category-book-box">
-            <Link to={`${props.title}`}>
-                <div id="book-image">
-                    <img src={`https://covers.openlibrary.org/b/isbn/${props.isbn}-S.jpg`} alt="Book cover image"/>
+                <div id="book-image-container">
+                    <Link id="image-link" to={`${props.title}`}>
+                        <img src={`https://covers.openlibrary.org/b/isbn/${props.isbn}-M.jpg`} alt="Book cover image"/>
+                    </Link>
                 </div>
-                <div id="book-info">
+                <div id="book-info-container">
                     <div id='title-author'>
-                        <h3>{props.title}</h3>
-                        <h4>{props.author_name}</h4>
+                        <Link id="title-link" to={`${props.title}`}>
+                            <h3>{props.title}</h3>
+                        </Link>
+                        <h4>by <Link id="author-link" to={`${props.author_name}`}>{props.author_name}</Link></h4>
                     </div>
-                    <p>{props.description}</p>
+                    <p>{preview} ...<Link id="description-link" to={`${props.title}`}>See more</Link></p>
                 </div>
-            </Link>
-
         </div>
     );
 
