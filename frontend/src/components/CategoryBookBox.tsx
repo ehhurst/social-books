1019804a374b6004b79455c4a13c6e0084b4ad37
ehurst96 <ehurst96@vt.edu>
2025-03-04 14:46:39 -0500

    added styling to display book boxes on the book page

--- frontend/src/components/CategoryBookBox.tsx
+++ frontend/src/components/CategoryBookBox.tsx
@@ -1,7 +1,8 @@
 import { BookItem } from "../types";
 import { Link } from "react-router-dom";
-import '../assets/css/CategoryBookBox.css'
 
+import '../assets/css/CategoryBookBox.css'
+import '../assets/css/global.css'
 
 
 function CategoryBookBox(props:BookItem) {
@@ -12,7 +13,7 @@ function CategoryBookBox(props:BookItem) {
         <div id="category-book-box">
                 <div id="book-image-container">
                     <Link id="image-link" to={`${props.title}`}>
-                        <img src={`https://covers.openlibrary.org/b/isbn/${props.isbn}-M.jpg`} alt="Book cover image"/>
+                        <img src={`https://covers.openlibrary.org/b/isbn/${props.work_ID}-M.jpg`} alt="Book cover image"/>
                     </Link>
                 </div>
                 <div id="book-info-container">
@@ -20,7 +21,7 @@ function CategoryBookBox(props:BookItem) {
                         <Link id="title-link" to={`${props.title}`}>
                             <h3>{props.title}</h3>
                         </Link>
-                        <h4>by <Link id="author-link" to={`${props.author_name}`}>{props.author_name}</Link></h4>
+                        <h4>by <Link id="author-link" to={`${props.author}`}>{props.author}</Link></h4>
                     </div>
                     <p>{preview} ...<Link id="description-link" to={`${props.title}`}>See more</Link></p>
                 </div>
