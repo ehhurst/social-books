677c7b90980853eab5f33dc59c9d308e52abd747
ehurst96 <ehurst96@vt.edu>
2025-03-04 18:29:00 -0500

    edited types.tsx to match backend API fields for book data

--- frontend/src/types.tsx
+++ frontend/src/types.tsx
@@ -1,16 +1,33 @@
+import { JSX } from "react";
+
 export interface BookItem {
-    isbn: string;
     title: string;
-    author_name: string;
+    author: string;
+    work_ID: string;
     description: string;
-    readTime: number;
+    img_S : string;
+    img_M : string,
+    img_L: string,
+    reading_Time: number;
 }
 
 export interface Review {
     reviewId: string;
     username: string;
-    isbn: number; 
     rating: number;
     reviewText: string;
     liked: boolean;
 }
+
+export const categories : string[] = [
+    "Fiction", 
+    "Poetry", 
+    "Fantasy", 
+    "History",
+    "Science fiction", 
+    "Programming"
+]
+
+export interface ContextProps {
+    children: JSX.Element | JSX.Element[]
+  }
