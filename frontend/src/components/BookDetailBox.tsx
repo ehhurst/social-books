53655f775a53f784d692b82bb4f2fc233e141152
ehurst96 <ehurst96@vt.edu>
2025-03-07 04:08:27 -0500

    created book page base with placeholder data

---
+++ frontend/src/components/BookDetailBox.tsx
@@ -0,0 +1,44 @@
+import { Link, useNavigate } from 'react-router-dom';
+import '../assets/css/BookDetailBox.css'
+
+import { BookItem } from '../types';
+
+function BookDetailBox(props:BookItem) {
+    const nav = useNavigate();
+
+
+    return(
+    <div id="container">
+        <div id="book-image-container">
+            <div>
+                <img src={`https://covers.openlibrary.org/b/id/6498519-M.jpg`} alt='Book Cover Image'/>
+            </div>
+        </div>
+        <div id="book-description">
+            <h1>{props.title}</h1>
+            <p>by</p>
+            <h2>{props.author}</h2>
+            <p id='description'>{props.description}</p>
+        </div>
+        <div id="book-buttons-and-stats">
+            <div id='stats-container'>
+                <table>
+                    <tbody>
+                        <td>Average Rating</td>
+                        <td>4.5</td>
+                        <td>Estimated Read Time</td>
+                        <td>{props.reading_Time}</td>
+                    </tbody>
+                </table>
+
+            </div>
+            <button className='secondary'>Mark as Read</button>
+            <button className='primary'>New Review</button>
+
+        </div>
+
+    </div>
+    );
+}
+
+export default BookDetailBox;
