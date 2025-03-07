0e1d28848aa5302dc4cad5fe7f02ea10e3a20d54
ehurst96 <ehurst96@vt.edu>
2025-03-07 04:11:55 -0500

    updated book object property calls

--- frontend/src/components/CategoryBookBox.tsx
+++ frontend/src/components/CategoryBookBox.tsx
@@ -3,27 +3,31 @@ import { Link } from "react-router-dom";
 
 import '../assets/css/CategoryBookBox.css'
 import '../assets/css/global.css'
+import { useEffect } from "react";
 
 
 function CategoryBookBox(props:BookItem) {
     const preview = props.description.slice(0, 150);
 
+    useEffect (() => console.log(props) )
+
 
     return(
         <div id="category-book-box">
+            
                 <div id="book-image-container">
-                    <Link id="image-link" to={`${props.title}`}>
+                    <Link id="image-link" to={`/book/${props.work_ID}`}>
                         <img src={props.img_M} alt="Book cover image"/>
                     </Link>
                 </div>
                 <div id="book-info-container">
                     <div id='title-author'>
-                        <Link id="title-link" to={`${props.title}`}>
+                        <Link id="title-link" to={`/book/${props.work_ID}`}>
                             <h3>{props.title}</h3>
                         </Link>
                         <h4>by <Link id="author-link" to={`${props.author}`}>{props.author}</Link></h4>
                     </div>
-                    <p>{preview} ...<Link id="description-link" to={`${props.description}`}>See more</Link></p>
+                    <p>{preview} ...<Link id="description-link" to={`/book/${props.work_ID}$`}>See more</Link></p>
                 </div>
         </div>
     );
