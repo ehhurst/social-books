08e19d95c4519b544cbfe9b36e52384cb687422e
ehurst96 <ehurst96@vt.edu>
2025-03-21 14:18:47 -0400

    styling fixes, added test review for UI

--- frontend/src/components/CategoryBookBox.tsx
+++ frontend/src/components/CategoryBookBox.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 
 
 function CategoryBookBox(props:BookItem) {
-    const preview = props.description.slice(0, 180);
+    const preview = props.description.slice(0, 150);
 
     useEffect (() => console.log(props) )
 
@@ -15,18 +15,18 @@ function CategoryBookBox(props:BookItem) {
     return(
         <div id="category-book-box">
                 <div id="book-cover-background">
-                    <Link id="image-link" to={`/book/${props.work_id}`}>
+                    <Link id="image-link" to={`/books/${props.work_id}`}>
                         <img id="cover" src={props.img_M} height={'115px'} alt="Book cover image"/>
                     </Link>
                 </div>
                 <div id="book-info-container">
                     <div id='title-author'>
-                        <Link id="title-link" to={`/book/${props.work_id}`}>
+                        <Link id="title-link" to={`/books/${props.work_id}`}>
                             <h3>{props.title}</h3>
                         </Link>
                         <h4>by <Link id="author-link" to={`${props.author}`}>{props.author}</Link></h4>
                     </div>
-                    <p>{preview} ...<Link id="description-link" to={`/book/${props.work_id}$`}>See more</Link></p>
+                    <p>{preview} ...<Link id="description-link" to={`/books/${props.work_id}`}>See more</Link></p>
                 </div>
         </div>
     );
