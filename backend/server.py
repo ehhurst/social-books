557abc64cbc211b0252cb810adf5c54305152035
ehurst96 <ehurst96@vt.edu>
2025-03-26 03:26:07 -0400

    changes formatting of returned json in return_review_data(work_id)

--- backend/server.py
+++ backend/server.py
@@ -14,6 +14,8 @@ CORS(app)  # Allow frontend to communicate with backend
 def home():
 	return jsonify({"message": "Flask API is running"}), 200
 
+## is this required anywhere? I think it was used for initial setup but no longer
+## called by frontend
 @app.route('/parse', methods=['POST'])
 def parse_book():
     try:
@@ -30,7 +32,7 @@ def parse_book():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-@app.route('/book', methods=['GET'])
+@app.route('/books', methods=['GET'])
 def book():
     """
     Flask route to handle book get requests.
@@ -218,6 +220,8 @@ def remove_review(review_id):
 # GET all reviews associated with a book
 @app.route("/books/<string:work_ID>/reviews", methods=["GET"])
 def return_review_data(work_ID):
+    print('here')
+    print(work_ID)
     # check for valid work ID
     if work_ID == '' or work_ID == "undefined":
         return jsonify({"error": f"book {work_ID} not found or invalid"}), 404 #NOT FOUND
@@ -239,21 +243,26 @@ def return_review_data(work_ID):
 
     # default return value for no reviews
     if len(reviews) == 0:
-        return ([dict(avg_rating=-1)] + [{}])
-
-    # Get average review score
-    query2 = """
+        avg = -1
+    else:
+        # Get average review score
+        query2 = """
         SELECT ROUND(AVG(star_rating), 1) FROM reviews
         WHERE reviews.work_id = ?
-    """
+        """
+        cursor = conn.execute(query2, (work_ID,))
+        singleRow = cursor.fetchone()
+        avg = [dict(avg_rating=singleRow[0])]
 
-    cursor = conn.execute(query2, (work_ID,))
-    singleRow = cursor.fetchone()
-    avg = [dict(avg_rating=singleRow[0])]
-    result = jsonify(avg + reviews)
+
+    reviews_data = {
+            'work_id': work_ID, 
+            'avg_rating': avg, 
+            'reviews_list': reviews
+    }
     conn.close()
 
-    return result
+    return jsonify(reviews_data)
 
 
 # GET all reviews associated with a user
