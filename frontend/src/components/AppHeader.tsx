cdcc698b8b05055f0f3e3a2208d11c34a7e899b0
ehurst96 <ehurst96@vt.edu>
2025-03-28 06:19:47 -0400

    added header storage of user login status, logout option

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -4,8 +4,21 @@ import AppLogo from '../assets/images/logo.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
 import SearchBar from "./SearchBar";
+import { useEffect } from 'react';
+import { faUser, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function AppHeader() {
     const nav = useNavigate();
+    const username = localStorage.getItem("username");
+    console.log("username" ,username);
+
+    function logOut(){
+        localStorage.removeItem("username");
+        localStorage.removeItem("access_token");
+        nav('/login');
+    }
+
+
     return (<header className="app-header">
             <Link to="/">
                 <img src={AppLogo} alt="Logo"/>
@@ -13,8 +26,16 @@ function AppHeader() {
             <div className='header-content'>
                 <SearchBar />  
                 <NavBar />
-                <button className='primary' onClick={() => nav("login")}>Sign In</button>
-                <button className='secondary' onClick={() => nav("register")}>Register</button>
+                {(username == null) ? 
+                <div>
+                    <button className='primary' onClick={() => nav('/login')}>Sign In</button>
+                    <button className='secondary' onClick={() => nav('/register')}>Register</button>
+                </div>
+                : <div id='user-info'>
+                    <Link to={'/reader-profile'}><FontAwesomeIcon id='user-icon' icon={faUserCircle} size={'xl'}/></Link>
+                    <p>Welcome, {username}</p>
+                    <Link to={'/login'} onClick={logOut}>Logout</Link>
+                </div>}
             </div>
         </header>);
 }
