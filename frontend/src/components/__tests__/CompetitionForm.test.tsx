435fb179692a82363005f03f7c25cd2b9d2acbef
bens21 <bens21@vt.edu>
2025-04-30 14:23:36 -0400

    added tests for events in CompetitionForm

--- frontend/src/components/__tests__/CompetitionForm.test.tsx
+++ frontend/src/components/__tests__/CompetitionForm.test.tsx
@@ -9,7 +9,6 @@ describe('CompetitionForm', () => {
               <CompetitionForm />
             </BrowserRouter>
           );
-    });
 
     expect(screen.getByRole('button', { name: / Back/i}).toBeInTheDocument());
     expect(screen.getByLabelText(/New Competiton/i).toBeInTheDocument());
@@ -20,4 +19,24 @@ describe('CompetitionForm', () => {
     expect(screen.getByRole('button', { name: /Remove/i}).toBeInTheDocument());
     expect(screen.getByRole('button', { name: /Create Competition/i}).toBeInTheDocument());
     
+    });
+
+    it('creates new competition', () => {
+      render(
+        <BrowserRouter>
+          <CompetitionForm />
+        </BrowserRouter>
+      )
+      const name_input = screen.getByLabelText(/Competition Name:/i);
+      fireEvent.change(name_input, { target: { value: 'Summer Competition' } });
+      expect(name_input.toHaveValue('Summer Competition'));
+
+      const end_input = screen.getByLabelText(/Competition end date:/i);
+      fireEvent.change(end_input, { target: { value: 'August 31, 2025' } });
+      expect(end_input.toHaveValue('August 31, 2025'));
+
+      fireEvent.click(screen.getByRole('Link', { name: /Add books /i}));
+
+      fireEvent.click(screen.getByRole('button', { name: /Create Competition/i }));
+  });
 });
