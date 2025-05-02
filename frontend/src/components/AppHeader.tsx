40dc7d8a72d8972e49778d2689d625c4271c34c8
ehurst96 <ehurst96@vt.edu>
2025-05-02 18:41:56 -0400

    AppHeader refactoring, code cleanup

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -4,35 +4,21 @@ import AppLogo from '../assets/images/logo.svg';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import NavBar from './NavBar';
 import SearchBar from "./SearchBar";
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useContext, useEffect } from 'react';
-import { AuthStore } from '../Contexts/AuthContext';
-import { User } from '../types';
+import { useEffect } from 'react';
 import HamburgerMenu from './HamburgerMenu';
 import UserLoginStatus from './UserLoginStatus';
+import { logout } from './utils/UserActions';
 
 function AppHeader() {
-    const nav = useNavigate();
     const par = useParams();
     const token = sessionStorage.getItem('access_token');
-    const currentUser:User = JSON.parse(sessionStorage.getItem('User') || "{}")
 
 
     useEffect(() => {
         if (!token && !par) {
-            sessionStorage.removeItem("creatingComp");
-            logOut();
+            logout();
         }
-    })
-
-
-    
-    function logOut(){
-        sessionStorage.removeItem('User');
-        sessionStorage.removeItem("access_token");
-        nav('/login');
-    }
+    }, [token])
 
     return (<header className="app-header">
             <Link to="/">
